d06a66e4d73ed809e9a585913db170a798cf8df5
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-08-31 15:09:10 +0530

    31/08/25

---
+++ EASY/customerorder.sql
@@ -0,0 +1,30 @@
+SELECT name as Customers FROM Customers
+WHERE id NOT IN (Select customerId from Orders)
+
+
+/*
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+*/
