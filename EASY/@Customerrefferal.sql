d6db0640bed662e0e76dfb55db04f1e0e29d8f99
Ayanika Paul <147071109+Ayanika0812@users.noreply.github.com>
2025-08-31 17:24:41 +0530

    Self join

---
+++ EASY/@Customerrefferal.sql
@@ -0,0 +1,34 @@
+SELECT c.name FROM Customer c
+JOIN 
+Customer r
+ON c.id = r.id
+WHERE r.referee_id IS null OR r.referee_id != 2
+
+/*
+Find the names of the customer that are either:
+referred by any customer with id != 2.
+not referred by any customer.
+Return the result table in any order.
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+*/
